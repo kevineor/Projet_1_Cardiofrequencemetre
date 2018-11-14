6ea0a0250692566390b0b22866363acabef5c997
Ticaiah <44462354+Ticaiah@users.noreply.github.com>
2018-11-14 14:09:44 +0100

    Rien de plus

--- Arduino/ProgrammeCoeur/param.h
+++ Arduino/ProgrammeCoeur/param.h
@@ -1,6 +1,6 @@
 #ifndef PARAM_H_INCLUDED
 #define PARAM_H_INCLUDED 
 // ce fichier va nous permettre de configurer la fa�on dont seront allume les LEDs 
-static int z=1; //On parametre le switch ici
+static int z=2; //On parametre le switch ici
 static int sel=0; // On param�tre la led � selectionner ici
 #endif // PARAM_H_INCLUDED
