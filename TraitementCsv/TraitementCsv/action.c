d07c92ca0f64097113d3d4e59608375f14cf7413
Kevin ANDRIEUX <kevin.andrieux@viacesi.fr>
2018-11-15 22:29:19 +0100

    Implementation algo recherche sequentielle

--- TraitementCsv/TraitementCsv/action.c
+++ TraitementCsv/TraitementCsv/action.c
@@ -20,3 +20,35 @@ int compare_by_pouls(Donnee *a, Donnee *b)
 	*/
 }
 
+
+//renvoie l'indice de la liste contenant la donnee, -1 si non trouvé
+int rechercherDate(Donnee * liste, int taille, int date)
+{
+	int trouve = -1;
+	int i;
+	for (i = 0; i < taille; i++)
+	{
+		if (liste[i].date == date)
+		{
+			trouve = i;
+			break;
+		}
+	}
+	return trouve;
+}
+
+int rechercherPouls(Donnee * liste, int taille, int pouls)
+{
+	int trouve = -1;
+	int i;
+	for (i = 0; i < taille; i++)
+	{
+		if (liste[i].pouls == pouls)
+		{
+			trouve = i;
+			break;
+		}
+	}
+	return trouve;
+}
+
