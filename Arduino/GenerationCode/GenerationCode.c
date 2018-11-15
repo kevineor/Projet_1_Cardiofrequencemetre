1bf97b8be483c5c6638c716d7b608b76fca263a1
Ticaiah <44462354+Ticaiah@users.noreply.github.com>
2018-11-15 09:23:38 +0100

    On a refait le switch

--- Arduino/GenerationCode/GenerationCode.c
+++ Arduino/GenerationCode/GenerationCode.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include "GenerationCode.h"
 
-void fon(int z,int sel)
+void Generation(int z,int sel)
 {
     FILE *f=fopen("C:\\Users\\vzonz\\Documents\\GitHub\\Projet_1_Cardiofrequencemetre\\Arduino\\ProgrammeCoeur\\param.h","w");
     fprintf(f ,"#ifndef PARAM_H_INCLUDED\n#define PARAM_H_INCLUDED \n// ce fichier va nous permettre de configurer la facon dont seront allume les LEDs \nstatic int z=%d; //On parametre le switch ici\nstatic int sel=%d; // On parametre la led a selectionner ici\n#endif // PARAM_H_INCLUDED", z,sel);
