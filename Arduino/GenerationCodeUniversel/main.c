9e0f4fee82db7c3a8452adcea3bf5baba33a6b11
Ticaiah <44462354+Ticaiah@users.noreply.github.com>
2018-11-16 13:16:02 +0100

    Code Arduino commenté + Code générateur C commenté

--- Arduino/GenerationCodeUniversel/main.c
+++ Arduino/GenerationCodeUniversel/main.c
@@ -4,7 +4,8 @@
 
 int main()
 {
-    int z,Pouls; //z sera la valeur commune pour le fichier "param.h", qui sélectionnera l'option dans le programme Arduino; et Pouls sera le nombre de BPM qu'on souhaitera simuler
+    int z;
+    int Pouls=0; //z sera la valeur commune pour le fichier "param.h", qui sélectionnera l'option dans le programme Arduino; et Pouls sera le nombre de BPM qu'on souhaitera simuler
     int sel=0; //Sel sera la variable pour sélectionner la LED unique à allumer avec l'Arduino, présente aussi dans "param.h"
     int menu=0; //Cette variable sera utilisée pour la sélection dans le menu et le switch
     printf("Bonjour!\n");
